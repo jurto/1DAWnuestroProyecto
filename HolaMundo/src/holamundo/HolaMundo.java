399cc5d535652e1c674fbf69df49bfd45bd26d0d
PixelZer0 <pixelzero@gmail.com>
2017-01-25 12:23:06 +0100

    Versión 1.0 subida

---
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -0,0 +1,17 @@
+package holamundo;
+
+/**
+ *
+ * @author Team
+ */
+public class HolaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo.");
+    }
+    
+}
